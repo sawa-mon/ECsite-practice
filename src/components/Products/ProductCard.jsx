73d8e10ac213d1d8b098197b050877fcf070e9a9
sawa-mon <sawada.6284@gmail.com>
2020-11-03 21:26:15 +0900

    値段、商品名紹介、編集、削除、一覧表示まで完了

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -7,6 +7,11 @@ import Typography from '@material-ui/core/Typography';
 import NoImage from '../../assets/img/src/no_image.png';
 import {push} from 'connected-react-router';
 import {useDispatch} from "react-redux";
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MOreVertIcon from '@material-ui/icons/MoreVert';
+import {deleteProduct} from '../../reducks/products/operations';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -41,6 +46,16 @@ const ProductCard = (props) => {
   const classes = useStyles();
   const dispatch = useDispatch();
 
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  };
+
   const images = (props.images.length > 0) ? props.images : [{path: NoImage}];
   const price = props.price.toLocaleString();
 
@@ -49,7 +64,7 @@ const ProductCard = (props) => {
     <Card className={classes.root}>
       <CardMedia
         className={classes.media}
-        image={props.images[0].path}
+        image={images[0].path}
         title=""
         onClick={() => dispatch(push('/product/' + props.id))}
       />
@@ -62,6 +77,32 @@ const ProductCard = (props) => {
             ¥{price}
           </Typography>
         </div>
+        <IconButton onClick={handleClick}>
+          <MOreVertIcon />
+        </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem
+              onClick={() => {
+                dispatch(push('/product/edit/' + props.id))
+                handleClose()
+              }}
+            >
+              編集する
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                dispatch(deleteProduct(props.id));
+                handleClose()
+              }}
+            >
+              削除する
+            </MenuItem>
+          </Menu>
       </CardContent>
     </Card>
   )
